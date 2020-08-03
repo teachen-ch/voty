1aeaaca6ae857f8b8b49278d3e3ef2102d46a876
Stefan N <stefan@flitz.ch>
2020-08-03 21:02:59 +0200

    Initial version of landing page

---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+import "chota";
+import "../styles/voty.css";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
---
+++ pages/index.tsx
@@ -0,0 +1,80 @@
+import Head from "next/head";
+// import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>voty – Demokratie an die Schule</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>voty</h1>
+        <h2>Demokratie an die Schule!</h2>
+
+        <div className="row">
+          <div className="col-12 col-4-md card bg-light">
+            <header>
+              <h3>Demokratie verstehen</h3>
+            </header>
+            <p>
+              Wie funktioniert die Demokratie in der Schweiz und warum ist sie
+              so wertvoll? Wir möchten Jugendliche von 12-18 Jahren motivieren,
+              sich mit unserem politischen System auseinanderzusetzen.
+            </p>
+            <button className="button primary is-full-width">
+              Anmeldung Newsletter
+            </button>
+          </div>
+          <div className="col-12 col-4-md  card bg-light">
+            <header>
+              <h3>Demokratie testen</h3>
+            </header>
+            <p>
+              Nur Erwachsene dürfen abstimmen, aber auch Jugendliche haben eine
+              Meinung! Wir möchten Test-Abstimmungen zeitgleich zu nationalen
+              Urnengängen durchführen und die Resultate diskutieren
+            </p>
+            <button className="button primary is-full-width">
+              Wir wollen auch!
+            </button>
+          </div>
+          <div className="col-12 col-4-md  card bg-light">
+            <header>
+              <h3>Demokratie erleben</h3>
+            </header>
+            <p>
+              Wer entscheidet an Eurer Schule? Dürfen Schülerinnen und Schüler
+              mitreden und Vorschläge einbringen? Wir möchten gemeinsam testen,
+              wie wir Demokratie in Schulhäuser bringen können.
+            </p>
+            <button className="button primary is-full-width">
+              Das interessiert uns!
+            </button>
+          </div>
+        </div>
+      </main>
+
+      <footer>
+        <p>
+          voty ist ein Projekt des{" "}
+          <a href="https://teachen.ch">«Verein Teachen!»</a>
+        </p>
+        <p>
+          <img src="/pf_logo.png" alt="Prototypefund Logo" className="logo" />
+          mit tatkräftiger Unterstützung durch den&nbsp;
+          <a href="https://prototypefund.opendata.ch" target="_blank">
+            PrototypeFund.ch
+          </a>
+        </p>
+
+        <nav>
+          <a href="#">Impressum</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+          <a href="#">Datenschutz</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+          <a href="#">Kontakt</a>
+        </nav>
+      </footer>
+    </div>
+  );
+}
