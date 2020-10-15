8b894ef63181a79c3374ae263bb9ea716ff6302a
Stefan N <stefnnn@users.noreply.github.com>
2020-10-15 20:35:38 +0200

    Load web stats only once

--- util/stats.ts
+++ util/stats.ts
@@ -1,22 +1,25 @@
-/* eslint-disable */
-// @ts-nocheck
+let initialized: boolean;
 
-export function initStats() {
-  var _paq = (window as any)._paq || [];
+export function initStats(): void {
+  if (initialized) return;
+  const _paq = [];
   /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
   _paq.push(["trackPageView"]);
   _paq.push(["enableLinkTracking"]);
   (function () {
-    var u = "https://stats.teachen.ch/";
+    const u = "https://stats.teachen.ch/";
     _paq.push(["setTrackerUrl", u + "matomo.php"]);
     _paq.push(["setSiteId", "2"]);
-    var d = document,
+    const d = document,
       g = d.createElement("script"),
       s = d.getElementsByTagName("script")[0];
     g.type = "text/javascript";
     g.async = true;
     g.defer = true;
     g.src = u + "matomo.js";
-    s!.parentNode.insertBefore(g, s);
+    if (s && s.parentNode) {
+      s.parentNode.insertBefore(g, s);
+      initialized = true;
+    }
   })();
 }
