91809f9aba27abc9918d5ae2742be1aaf75d1a6f
Stefan N <stefnnn@users.noreply.github.com>
2020-11-24 16:47:42 +0100

    Struggling with ipv6 email

--- util/email.ts
+++ util/email.ts
@@ -12,6 +12,7 @@ const server = new SMTPTransport({
     user: process.env.SMTP_USER || "",
     pass: process.env.SMTP_PASSWORD || "",
   },
+  tls: { rejectUnauthorized: false },
 });
 
 const templatesDir = "./mails/";
