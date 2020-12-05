d9f010c53e18f56a707b1a6eae49e27daca24ee2
Stefan N <stefnnn@users.noreply.github.com>
2020-12-05 20:47:49 +0100

    Change MDX wrapping behaviour

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -38,6 +38,22 @@ function MyApp({ Component, pageProps }: AppProps): ReactElement {
   );
 }
 
-const MDXWrapper: React.FC = (props) => <Page>{props.children}</Page>;
+// this will wrap the MDX into a <Page> only if there is a heading (# title)
+const MDXWrapper: React.FC = ({ children }) => {
+  let hasHeadings = false;
+  if (children && Array.isArray(children)) {
+    const headings = children.filter((el: React.ReactNode) => {
+      if (el && typeof el === "object" && "props" in el)
+        return el.props.mdxType === "h1";
+      else return false;
+    });
+    hasHeadings = headings.length > 0;
+  }
+  if (hasHeadings) {
+    return <Page>{children}</Page>;
+  } else {
+    return <>{children}</>;
+  }
+};
 
 export default MyApp;
