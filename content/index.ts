bd67679ce5c7266006535ea5986c42ea3c57e9f7
Stefan N <stefnnn@users.noreply.github.com>
2020-12-07 12:21:29 +0100

    Demo/draft content for Learning Cards

---
+++ content/index.ts
@@ -0,0 +1,7 @@
+export * as srf_3_gewalten from "content/videos/srf_3_gewalten.mdx";
+export * as srf_foederalismus from "content/videos/srf_foederalismus.mdx";
+export * as srf_mitbestimmen from "content/videos/srf_mitbestimmen.mdx";
+export * as srf_usa_parteien from "content/videos/srf_usa_parteien.mdx";
+export * as srf_waehlen from "content/videos/srf_waehlen.mdx";
+export * as chaty_initiativen from "content/chaty/chaty_initiativen.mdx";
+export * as chaty_referendum from "content/chaty/chaty_referendum.mdx";
