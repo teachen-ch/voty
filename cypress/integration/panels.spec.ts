cccda86a9b3f83c5b1d776aaec47d03eab05716f
Stefan N <stefnnn@users.noreply.github.com>
2020-12-12 17:15:14 +0100

    Still trying to fix e2e test

--- cypress/integration/panels.spec.ts
+++ cypress/integration/panels.spec.ts
@@ -3,23 +3,22 @@ describe("Test Live-Voting Panels", () => {
     cy.task("prismaLoader", "testdb.yml");
   });
 
-  // TODO: need to reimplement after redesign of selection flow
   it("opens teacher page and selects some ballots", () => {
     cy.login();
     cy.visit("/teacher");
     cy.contains("Class 1").click();
-    // add testinitiative
     cy.contains("Folgende Abstimmungen");
+    cy.wait(1000);
+    // add testinitiative
     cy.get(
       "#ballots tr:contains('Testinitiative') svg[alt='abgewählt']"
     ).click();
     cy.get("#ballots tr:contains('Testinitiative') svg[alt='ausgewählt']");
 
-    // TODO: where is this button supposed to be?
-    // cy.get("#livepanel button").first().click();
-    // cy.contains("Jetzt abstimmen");
+    // go start ballotRun
     cy.visit("/panel/1999999/present");
     cy.get(".ballot:first button:contains('Starten')").click();
+
     // logout user
     cy.visit("/user/logout");
     cy.contains("Anmelden");
