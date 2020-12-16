76b0dd5f58a4d403805a6df2f97cce4aa8c5340c
Stefan N <stefnnn@users.noreply.github.com>
2020-12-16 16:34:00 +0100

    Enhance simple markdown parsing

--- @types/mdx.d.ts
+++ @types/mdx.d.ts
@@ -3,3 +3,50 @@ declare module "*.mdx" {
   export const meta: Record<string, any>;
   export default MDXComponent;
 }
+
+declare module "@mdx-js/react" {
+  import * as React from "react";
+  type ComponentType =
+    | "a"
+    | "blockquote"
+    | "code"
+    | "del"
+    | "em"
+    | "h1"
+    | "h2"
+    | "h3"
+    | "h4"
+    | "h5"
+    | "h6"
+    | "hr"
+    | "img"
+    | "inlineCode"
+    | "li"
+    | "ol"
+    | "p"
+    | "pre"
+    | "strong"
+    | "sup"
+    | "table"
+    | "td"
+    | "thematicBreak"
+    | "tr"
+    | "ul";
+  export type Components = {
+    [key in ComponentType]?: React.ComponentType<any>;
+  };
+  export interface MDXProviderProps {
+    children: React.ReactNode;
+    components: Components;
+  }
+  export class MDXProvider extends React.Component<MDXProviderProps> {}
+}
+
+declare module "@mdx-js/runtime" {
+  export interface MDXProps {
+    children: React.ReactNode;
+    components?: Components;
+    scope?: Record<string, any>;
+  }
+  export default class MDX extends React.Component<MDXProps> {}
+}
--- util/markdown.tsx
+++ util/markdown.tsx
@@ -1,3 +1,9 @@
+export const Markdown: React.FC<{}> = ({ children }) => (
+  <div
+    dangerouslySetInnerHTML={parseMarkdownInner(String(children))}
+    style={{ textAlign: "left" }}
+  />
+);
 // TODO: this is a joke markdown parser after being frustrated with mdx-js
 export function parseMarkdown(str: string): string {
   str = str.replace(/^\s*#\s+(.*?)$/gm, "<h1>$1</h1>");
@@ -5,7 +11,10 @@ export function parseMarkdown(str: string): string {
   str = str.replace(/^\s*###\s+(.*?)$/gm, "<h3>$1</h3>");
   str = str.replace(/^\s*####\s+(.*?)$/gm, "<h4>$1</h4>");
   str = str.replace(/^\s*-\s+(.*?)$/gm, "<li>$1</li>");
+  str = str.replace(/\*\*(.*?)\*\*/gm, "<b>$1</b>");
+  str = str.replace(/\*(.*?)\*/gm, "<i>$1</i>");
   str = str.replace(/\n/g, "<br/>");
+  str = str.replace(/\!\[(.*?)\]\(([^\s]*?)\)/g, "<img src='$2' alt='$1'/>");
   str = str.replace(/(<\/h\d>)<br\/>\s*/g, "$1");
   str = str.replace(/\[(.*?)\]\((.*?)\)/g, '<a href="$2">$1</a>');
   return str;
