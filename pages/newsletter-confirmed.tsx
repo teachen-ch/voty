b46c5b5c08e650453cf2dd3a6b7e62b4b6aaa073
Stefan N <stefnnn@users.noreply.github.com>
2020-12-29 19:41:35 +0100

    Implements #84 with newsletter confirmation page

---
+++ pages/newsletter-confirmed.tsx
@@ -0,0 +1,21 @@
+import { Page } from "components/Page";
+import { Flex, Heading } from "rebass";
+import { ReactElement } from "react";
+
+export default function NewsletterConfirmed(): ReactElement {
+  return (
+    <Page heading="Newsletter-Anmeldung">
+      <Flex
+        flexDirection="column"
+        minHeight="450px"
+        sx={{ background: 'url("/images/voty_welcome.svg") center no-repeat' }}
+      >
+        <Heading as="h2" id="newsletter">
+          ✅ Super, deine Newsletter-Anmeldung ist jetzt bestätigt!
+        </Heading>
+        Du kannst Dich jederzeit wieder von Newsletter abmelden, wenn Du auf den
+        entsprechenden Link im Newsletter klickst.
+      </Flex>
+    </Page>
+  );
+}
