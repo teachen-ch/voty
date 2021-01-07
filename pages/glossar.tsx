e8a916167873fbf09ab77f2a64179492f619c640
Stefan N <stefnnn@users.noreply.github.com>
2021-01-07 07:48:38 +0100

    Some additional glossary entries

--- pages/glossar.tsx
+++ pages/glossar.tsx
@@ -1,11 +1,22 @@
 import { Glossary } from "components/Glossary";
 import { Text } from "rebass";
 import { Page } from "components/Page";
+import Info from "components/Info";
 
 export default function GlossaryPage(): React.ReactElement {
+  function sendMail() {
+    const email = "hc.ytoy@rassolg:otliam";
+    document.location.href = email.split("").reverse().join("");
+  }
   return (
     <Page heading="Begriffe der Demokratie">
       <Text textAlign="left">
+        <Info>
+          Dieses Glossar ist in einer frühen Phase der Entstehung und hat weder
+          Anspruch auf Vollständigkeit noch auf Korrektheit. Bitte schickt
+          Ergänzungs- und Präsizierungsvorschläge an ✉️
+          <a onClick={sendMail}>glossar@voty.ch</a>
+        </Info>
         <Glossary />
       </Text>
     </Page>
