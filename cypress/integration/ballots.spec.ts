6b8de57b08f58fa0818bb37bbe80cef4cf3ae9ca
Stefan N <stefnnn@users.noreply.github.com>
2021-01-14 12:29:15 +0100

    Eliminating more Test Flake

--- cypress/integration/ballots.spec.ts
+++ cypress/integration/ballots.spec.ts
@@ -3,28 +3,11 @@ describe("Test Ballots", () => {
     cy.task("prismaLoader", "testdb.yml");
   });
 
-  it("lets student vote on a ballot (only once)", () => {
-    cy.login("student@teachen.ch", "teachen");
-    cy.visit("/ballots/");
-    cy.contains("Testinitiative").click();
-    cy.contains("Ja, ich stimme zu").click();
-    cy.contains("Du hast abgestimmt");
-    cy.visit("/user/logout");
-
-    cy.login("student@teachen.ch", "teachen");
-    cy.visit("/ballots/");
-    cy.contains("Abgelaufene Initiative").click();
-    cy.contains("Abstimmung ist beendet");
-    cy.go("back");
-
-    cy.contains("Zukunftsinitiative").click();
-    cy.contains("Abstimmung noch nicht gestartet");
-  });
-
   it("opens teacher page and selects some ballots", () => {
     cy.login();
     cy.visit("/teacher");
     cy.contains("Class 1").click();
+    cy.contains("Abstimmungen");
     cy.get("#ballots tr:first td:last").click();
     cy.get("#ballots tr:first td:last svg[alt='ausgewählt']");
     // select and de-select last ballot
@@ -36,5 +19,20 @@ describe("Test Ballots", () => {
     cy.visit("/student");
     cy.contains("Testinitiative").click();
     cy.contains("Ja, ich").click();
+    cy.contains("Du hast abgestimmt");
+    cy.visit("/user/logout");
+
+    cy.login("student@teachen.ch", "teachen");
+    cy.visit("/ballots/");
+
+    cy.contains("Testinitiative").click();
+    cy.contains("Du hast erfolgreich");
+    cy.go("back");
+    cy.contains("Abgelaufene Initiative").click();
+    cy.contains("Abstimmung ist beendet");
+    cy.go("back");
+    cy.contains("Zukunftsinitiative").click();
+    cy.contains("Abstimmung noch nicht");
+    cy.go("back");
   });
 });
