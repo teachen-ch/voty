b3cbe80b2e172765ed389763e376008ba114c526
Stefan N <stefnnn@users.noreply.github.com>
2021-03-04 17:54:04 +0100

    feat: implement #125 ballot comparison

--- pages/team/[team]/[ballot_id].tsx
+++ pages/team/[team]/[ballot_id].tsx
@@ -1,8 +1,8 @@
-import { LoggedInPage, AppPage, ErrorPage } from "components/Page";
-import { Text, Box } from "rebass";
+import { LoggedInPage, AppPage, ErrorPage, H2, H3 } from "components/Page";
+import { Text, Box, Flex } from "rebass";
 import { useRouter } from "next/router";
 import { Role, useBallotQuery, useGetBallotResultsQuery } from "graphql/types";
-import { BallotResults } from "components/BallotResults";
+import { BallotResults, VotyPie } from "components/BallotResults";
 import { BallotDetails } from "components/Ballots";
 import { usePageEvent } from "util/stats";
 import { Breadcrumb, A, Here } from "components/Breadcrumb";
@@ -10,6 +10,8 @@ import { usePolling } from "util/hooks";
 import { Discussion } from "components/Discussion";
 import { HideFeature } from "components/HideFeature";
 import { useTeam } from "state/user";
+import { useState } from "react";
+import { Grid } from "theme-ui";
 
 export default function TeacherBallotPage(): React.ReactElement {
   usePageEvent({ category: "Teacher", action: "BallotDetails" });
@@ -25,7 +27,13 @@ export default function TeacherBallotPage(): React.ReactElement {
     variables: { ballotId, teamId: team?.id },
     skip: !(ballotId && team?.id),
   });
+  const compareQuery = useGetBallotResultsQuery({
+    variables: { ballotId },
+    skip: !(ballotId && team?.id),
+  });
   const results = resultsQuery.data?.getBallotResults;
+  const compare = compareQuery.data?.getBallotResults;
+  const [comparison, setComparison] = useState(false);
   usePolling(resultsQuery);
 
   if (ballotQuery.loading || !team)
@@ -49,13 +57,38 @@ export default function TeacherBallotPage(): React.ReactElement {
         <Here>{ballot.title}</Here>
       </Breadcrumb>
       {!results?.total ? (
-        ""
+        <Box bg="#fff" padding={4} color="gray" fontStyle="italic">
+          Hier werden später die Resultate Deiner Klasse angezeigt
+        </Box>
       ) : (
         <Box my={4}>
           <Text mb={4}>
             Hier sind die aktuellen Resultate für die Klasse «{team.name}»:
           </Text>
           <BallotResults results={results} />
+          {compare && compare.total && compare.total > 50 && (
+            <Box mt={3}>
+              <A onClick={() => setComparison(!comparison)} fontSize={2}>
+                Vergleich mit allen Jugend-Stimmen auf voty.ch
+              </A>
+              {comparison && (
+                <Box textAlign="center" mt={4}>
+                  <Text fontWeight="semi">
+                    Aktuelles Zwischenresultat aller Jugend-Stimmen auf voty.ch
+                  </Text>
+                  <Box
+                    mt={4}
+                    mx={"auto"}
+                    height={200}
+                    width={200}
+                    sx={{ backgroundColor: "#fff", borderRadius: 100 }}
+                  >
+                    <VotyPie results={compare} />
+                  </Box>
+                </Box>
+              )}
+            </Box>
+          )}
         </Box>
       )}
       <BallotDetails ballot={ballot} />
