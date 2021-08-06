9a0ea57260e31b64c9755a55fc082a8a72edf830
Stefan N <stefnnn@users.noreply.github.com>
2021-08-06 14:25:00 +0200

    feat: add language config to next for routing

--- next.config.js
+++ next.config.js
@@ -4,6 +4,12 @@ const withMDX = require("@next/mdx")(/* { extension: /\.(md|mdx)$/,} */);
 module.exports = withMDX({
   pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
   reactStrictMode: true,
+  // i18n
+  i18n: {
+    locales: ["de", "fr", "it"],
+    defaultLocale: "de",
+    localeDetection: false,
+  },
   // Next 10 image conf: https://nextjs.org/docs/basic-features/image-optimization
   images: {
     deviceSizes: [400, 600, 1200, 2000],
