ebb05f582163d23ba33326af42d8c6e21ec4dd1d
Stefan N <stefnnn@users.noreply.github.com>
2021-10-30 16:32:46 +0200

    feat: content changes for module 3

--- pages/leben.tsx
+++ pages/leben.tsx
@@ -18,22 +18,22 @@ export default function LebenPage(): React.ReactElement {
       </Text>
       <H2>Konzept</H2>
       <Text>
-        Wir möchten auf voty.ch den demokratischen Entscheidungsprozess in die
-        Schule bringen. Schulleitung, Lehrerschaft und Schüler*Innen führen
-        gemeinsam einen offenen und kreativen Dialog über schulinterne Ideen und
-        Vorschläge. Schüler*innen können in einem mit der Schulleitung
-        vereinbarten Rahmen («Vertrag») auf der Platform eigene Ideen
-        («Initiativen») formulieren, diese gemeinsam off- und online
-        diskutieren, verbessern, final ausarbeiten und dafür Mehrheiten finden.
-        Nach der Prüfung durch die Schulleitung (Machbarkeit und
-        Vertragskonformität) gelangt der ausgearbeitete Vorschlag zur Abstimmung
-        und wird bei einer Annahme realisiert. Dabei werden die Schüler\*Innen
-        didaktisch begleitet, um den Prozess gemeinsam zu reflektieren.
+        Gemeinsam mit aula.de aus Deutschland möchten wir den demokratischen
+        Entscheidungsprozess in schweizer Schulen bringen. Schulleitung,
+        Lehrerschaft und Schüler*Innen führen gemeinsam einen offenen und
+        kreativen Dialog über schulinterne Ideen und Vorschläge. Schüler*innen
+        können in einem mit der Schulleitung vereinbarten Rahmen («Vertrag») auf
+        der Platform eigene Ideen («Initiativen») formulieren, diese gemeinsam
+        off- und online diskutieren, verbessern, final ausarbeiten und dafür
+        Mehrheiten finden. Nach der Prüfung durch die Schulleitung (Machbarkeit
+        und Vertragskonformität) gelangt der ausgearbeitete Vorschlag zur
+        Abstimmung und wird bei einer Annahme realisiert. Dabei werden die
+        Schüler\*Innen didaktisch begleitet, um den Prozess gemeinsam zu
+        reflektieren.
       </Text>
       <Info type="info" py={3}>
-        Denken sie darüber nach, an ihrer Schule partizipative Prozessen
-        einzuführen oder haben sie bereits Erfahrungen damit gesammelt? Dann
-        würden wir gerne mit ihnen ins Gespräch kommen!
+        Wollen sie an ihrer Schule partizipative Prozessen einzuführen? Dann
+        melden sie sich gerne bei uns!
         <br />
         <br />
         <NewsletterSlim campaign="modul3-leben" />
