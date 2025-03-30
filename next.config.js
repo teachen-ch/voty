c52b9c84b437238fc4a2cc47956f1b46fd2b96c3
Stefan N <stefnnn@users.noreply.github.com>
2025-03-30 10:42:34 +0200

    chore: next update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-// eslint-disable-next-line
 const withMDX = require("@next/mdx")(/* { extension: /\.(md|mdx)$/,} */);
-// eslint-disable-next-line
+
 module.exports = withMDX({
   pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
   reactStrictMode: true,
